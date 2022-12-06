d3c73b9dd1eda96dc34dfeb4d999c0aea8a615c9
Lewis Benson <tm.lewis.benson@gmail.com>
2022-12-06 17:11:27 -0500

    add missing semi-colons

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -11,12 +11,12 @@ module.exports = async (req, res, next) => {
   let [username, pass] = base64.decode(basic).split(':');
 
   try {
-    req.user = await user.authenticateBasic(username, pass)
+    req.user = await user.authenticateBasic(username, pass);
     next();
   } catch (e) {
     console.error(e);
     res.status(403).send('Invalid Login');
   }
 
-}
+};
 
