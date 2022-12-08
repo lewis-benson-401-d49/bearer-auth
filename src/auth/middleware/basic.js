84d3890364ecc7e547af89f71961151cf2f6abf0
Lewis Benson <tm.lewis.benson@gmail.com>
2022-12-08 00:41:27 -0500

    move codeblock inside of try{}

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -4,13 +4,13 @@ const base64 = require('base-64');
 const { users } = require('../models/index.js');
 
 module.exports = async (req, res, next) => {
-
-  if (!req.headers.authorization) { next('Invalid Login'); }
-  let basic = req.headers.authorization.split(' ');
-
-  let [username, pass] = base64.decode(basic[1]).split(':');
-
   try {
+    if (!req.headers.authorization) { next('Invalid Login'); }
+    let basic = req.headers.authorization.split(' ');
+
+    let [username, pass] = base64.decode(basic[1]).split(':');
+
+
     req.user = await users.authenticateBasic(username, pass);
     next();
   } catch (e) {
