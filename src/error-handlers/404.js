edf72b6e307f127998b7770d1a623623844944c1
Lewis Benson <tm.lewis.benson@gmail.com>
2022-12-08 00:41:38 -0500

    add error first to middleware

--- src/error-handlers/404.js
+++ src/error-handlers/404.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = (req,res,next) => {
+module.exports = (err, req, res, next) => {
   let error = { error: 'Resource Not Found' };
   res.statusCode = 404;
   res.statusMessage = 'Not Found';
